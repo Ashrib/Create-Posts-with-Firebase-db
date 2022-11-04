4a7ca5523c911f312a8718a611daed4670eaa29d
Syed Kariz Ali <karizali310@gmail.com>
2022-11-04 16:38:34 +0500

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import './App.css';
+import profilePic from "./images/profile.jpg";
 // import axios from 'axios';
 import moment from 'moment';
 import { initializeApp } from "firebase/app";
 import {
    getFirestore, collection, addDoc, 
    getDocs, doc, onSnapshot, query,
-   updateDoc, serverTimestamp, orderBy, limit
+   updateDoc, serverTimestamp, orderBy, limit,
+   deleteDoc
   } from "firebase/firestore";
  
 const firebaseConfig = {
@@ -25,15 +27,11 @@ const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-
 function App() {
 
   const [postText, setPostText] = useState("");
   const [posts, setPosts] = useState([]);
   // const [isLoading, setIsLoading] = useState(false);
-
-
-
 
   useEffect(() => {
       const getData = async() => {
@@ -53,7 +51,7 @@ function App() {
       unsubscribe = onSnapshot(q, (querySnapshot) => {
       const allPosts = [];
       querySnapshot.forEach((doc) => {
-        allPosts.push(doc.data());
+        allPosts.push({...doc.data(), ...doc.id});
       });
       setPosts(allPosts);
       console.log("Posts: ", allPosts);
@@ -61,15 +59,12 @@ function App() {
     }
     getRealTimeData();
 
-
     ///unsubscribtion
     return () => {
     unsubscribe();
     }
     
   }, [])
-
-
 
 
   const createPost = async (e) => {
@@ -92,13 +87,17 @@ function App() {
       }
     }
     
-    
   }
+  const deletePost = async (postId) => {
+    await deleteDoc(doc(db, "posts", postId));
+  };
 
 
   return (
-    <div>
-
+      <>
+    <div className="container">
+      {/* post create box */}
+      <div className="create-box">
       <form onSubmit={createPost}>
         <input
           type="text"
@@ -109,20 +108,37 @@ function App() {
         />
         <button type="submit">post</button>
       </form>
+      </div>
+      {/* post create box end*/}
 
-      <div>
+      <div className="allposts-box">
         {/* {(isLoading) ? "loading..." : ""} */}
 
         {posts.map((eachPost, i) => (
           <div className="post" key={i}>
+            <div className="post-head">
+              <div className="post-info">
+              <img src={profilePic} alt="profile" />
+              <div className="post-head-texts">
+                <span className="user-name">Asharib Ali</span>
+                <span className="post-time">
+                  {
+                  moment((eachPost?.createdOn?.seconds) ? 
+                  eachPost?.createdOn?.seconds*1000 : 
+                  undefined)
+                    .format('Do MMMM, h:mm a')
+                  }
+                </span>
+              </div>
+              </div>
+              <div className="post-options" onClick={()=> {
+                deletePost(eachPost?.id);
+              }}>gggg</div>
+            </div>
 
              
             <h3>{eachPost?.text}</h3>
 
-            <span>{
-              moment(eachPost?.createdOn?.seconds*1000)
-                .format('Do MMMM, h:mm a')
-            }</span>
 
 
              
@@ -132,6 +148,7 @@ function App() {
       </div>
 
     </div>
+    </>
   );
 }
 
